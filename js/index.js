b7e091437fff132fc6216bd60cd24ce6b0c0c82b
Zachary Gooding <zac.gooding@gmail.com>
2021-04-07 03:30:56 -0400

    4 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,66 @@
 // Your code goes here
+  
+Array.from(document.links).forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+  });
+
+// Body
+const body = document.querySelector('body');
+body.addEventListener('click', function(event){
+    event.target.style.background = 'grey';
+    setTimeout(function(){
+        event.target.style.background ="";
+    }, 3000);
+},false);
+
+
+// Header
+const header = document.querySelector('header');
+header.addEventListener('mouseover', function(event){
+    event.currentTarget.style.background = 'red';
+    event.target.style.color = 'white'
+    setTimeout(function(){
+        event.target.style.color = "";
+}, 1000);
+}, false);
+// console.log(header);
+
+
+// Intro Image
+const introImg = document.querySelector('.intro img');
+introImg.addEventListener('pointerdown', function(event){
+    event.target.style.transition = 'all 1s ease-out';
+    event.target.style.transform = 'scale(1.5)';
+    setTimeout(function(){
+        event.target.style.transform = "";
+    }, 2000);
+}, false);
+
+// introImg.addEventListener('mouseout', function(event){
+//     event.target.style.transform = 'scale(1)';
+// });
+
+// Map Image
+const mapImg = document.querySelector('.img-content img');
+const count = 360;
+mapImg.addEventListener('dblclick', function(event){
+    event.target.style.transition = 'all 1s';
+    event.target.style.transform = `rotate(${count}deg)`;
+    count += 360;
+});
+
+// Text Content 
+
+const textCont = document.querySelector('.text-content:nth-child(1)');
+textCont.addEventListener('mouseenter', function(event){
+    event.target.style.transition = 'all 1s'
+    event.target.style.transform = 'scale(1)';
+   
+});
+console.log(textCont);
+
+textCont.addEventListener('mouseout', function(event){
+    event.target.style.transform = 'scale(1)';
+});
