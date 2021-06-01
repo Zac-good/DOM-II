bf22c2a4e4a42c1edcf0439ce603cca538d20cbd
Zachary Gooding <zac.gooding@gmail.com>
2021-06-01 16:19:19 -0400

    Added event listener to the body

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+Array.from(document.links).forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+  });
+
+  // Body
+const body = document.querySelector('body');
+body.addEventListener('click', function(event){
+    event.target.style.background = 'grey';
+    setTimeout(function(){
+        event.target.style.background ="";
+    }, 3000);
+},false);
